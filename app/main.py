4f2c593eadd36b22ca6c24ba6213de7be1ca93a4
JaVi <javairialateef627@gmail.com>
2026-07-26 22:35:31 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
+
+    builtin_commands=["type","echo","exit"]
     while True:
         sys.stdout.write("$ ")
         
@@ -12,7 +14,13 @@ def main():
 
         elif command.startswith("echo"):
              print(" ".join(command.split()[1:]))
-             
+
+        elif command.startswith("type"):
+             for b in builtin_commands:
+                  if command==b:return {f"{command} is a shell builtin"}
+                  else:print(f"{command}: not found")
+                       
+                 
         else:
              print(f"{command}: command not found")
 
