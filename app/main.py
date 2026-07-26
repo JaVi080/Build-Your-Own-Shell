f54898418b3fa4ea72090121d3f1cd520d1f354f
JaVi <javairialateef627@gmail.com>
2026-07-26 20:59:18 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
+        
+        command=input()
+        print(f"{command}: command not found")
     
-    command=input()
-    print(f"{command}: command not found")
-
-
 
 if __name__ == "__main__":
     main()
