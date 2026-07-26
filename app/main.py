d21b03cf09437e3e1c47eed8031be0533755fee9
JaVi <javairialateef627@gmail.com>
2026-07-26 22:41:28 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -18,8 +18,9 @@ def main():
 
         elif command.startswith("type"):
              for b in builtin_commands:
-                  if command[5:]==b:return {f"{command[5:]} is a shell builtin"}
-                  else:print(f"{command}: not found")
+                  if command[5:]==b:return print(f"{command[5:]} is a shell builtin")
+
+             else:print(f"{command}: not found")
                        
                  
         else:
