5a8441e9901d5862c507c95dc8bb9bc70e63f581
JaVi <javairialateef627@gmail.com>
2026-07-31 12:15:09 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,48 +7,54 @@ class Shell:
     # builtin_commands=["type","echo","exit"]  
     def __init__(self,command):
         self.command=command
-        self.__path=os.environ.get("PATH").split(os.pathsep)
+        self.__path=os.environ.get("PATH","").split(os.pathsep)
         self.builtin_commands=["type","echo","exit"] 
+        
+
+    def execute(self):
+         parts = self.command.split()
+         if not parts:return 
+         cmd= parts[0] 
+
+         start_Methods={
+                "echo":lambda:self.echo,
+                "type":lambda:self.type_m,
+                "exit":lambda:sys.exit(0)
+               
+            }
+
+         select_method=start_Methods.get(cmd,lambda:self.External)
+
+         select_method()
+
 
     def echo(self):print(" ".join(self.command.split()[1:]))
 
-    def type_m(self,command)->None:
-        for b in self.builtin_commands:
-            if command[5:]==b:
-                print(f"{command[5:]} is a shell builtin")
-                break
-                #else  is of for loop 
-        else:
+    def type_m(self)->None:
+        parts = self.command.split()
 
-                          #PATH env variable
+        if parts[1] in self.builtin_commands:
+                print(f"{parts[1]} is a shell builtin")
+                return
+
+                     #PATH env variable
             # path =os.environ.get("PATH").split(os.pathsep)
-            for dictionary in self.__path:
-                filePath=os.path.join(dictionary,self.command[5:])
+        for dictionary in self.__path:
+                filePath=os.path.join(dictionary,parts[1])
                 if os.access(filePath, os.X_OK):                
-                    print(f"{command[5:]} is {filePath}")
-                    break
+                    print(f"{parts[1]} is {filePath}")
+                    return
 
-            else:
-                print(f"{command[5:]}: not found")
+        print(f"{parts[1]}: not found")
 
-    def External():
-         if file:=shutil.which(command.split()[0]):
+    def External(self):
+         if file:=shutil.which(self.command.split()[0]):
                             #subprocess.run() expects the command and its arguments as separate items.
-            subprocess.run(command.split())
+            subprocess.run(self.command.split())
         
-         else:  print(f"{command}: not found")
+         else:  print(f"{self.command}: not found")
 
 
-
-
-    start_Methods={
-        "echo":lambda:echo(self.comman),
-        "type":lambda:type_m(self.comman),
-        "exit":lambda:sys.exit(0)
-       
-    }
-    cmd=command.split()[0] if command.split()>0 else ""
-    select_method=start_Methods.get(cmd,External())
 # this all process can be replaced by shutil.which(cmd)
       #    bcz internally it do all this like 
  # shutil.which(command) internally does something very similar to:
@@ -63,15 +69,12 @@ class Shell:
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-
-
     while True:
         sys.stdout.write("$ ")
         
         command=input()
         s=Shell(command)
-
-
+        s.execute()
 
 if __name__ == "__main__":
     main()
